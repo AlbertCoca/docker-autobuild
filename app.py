058d8fb0b8cf912b775cd0b2de4fe26596d64998
AlbertCoca <albert.cocaabello@bsc.es>
2019-05-29 14:50:21 +0200

    Added main app

---
+++ app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# ==== IMPORTS SECTION ========================================================
+import flask
+# ==== CONSTANTS DEFINITIONS ==================================================
+
+# ==== CLASS DEFINITION =======================================================
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
